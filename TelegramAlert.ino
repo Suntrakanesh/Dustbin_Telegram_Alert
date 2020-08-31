2a8eabadd3032f4da4f8067f06b9302d87e62edb
Suntrakanesh SU <64604283+Suntrakanesh@users.noreply.github.com>
2020-08-31 18:18:30 +0530

    Create TelegramAlert.ino

---
+++ TelegramAlert.ino
@@ -0,0 +1,58 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <TelegramBot.h>
+
+const int trigPin = 9; //trigPin is connected to pin 9
+const int echoPin = 10; //echoPin is connected to pin 10
+
+// defines variables
+  
+long duration;
+int distance;
+
+const char* ssid = "<Your WiFi Name or SSID>"; //Enter the WiFi name where you need to connect
+const char* password = "<Your WiFi Password>"; //Enter the password as well
+
+const char BotToken[] = "978892373:AAHBH3TJsS5loT6mHEfw7wS79M_wO4Rp1C4"; //Bot token which is the token used to send alert message in telegram
+
+WiFiClientSecure net_ssl;
+TelegramBot bot (BotToken, net_ssl);
+
+void setup()
+{
+
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
+
+  Serial.begin(115200);
+  while (!Serial) {} //Start running when the serial is open
+  delay(3000);
+  Serial.print("Connecting WiFi.");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, password) != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  bot.begin(); // Begins the bot in telegram
+}
+void loop() 
+{  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
+  distance= duration*0.034/2; // Calculating the distance
+  
+  //Checking the distance is less than 6 cm, if it so, then alert message will send to the telegram channel
+  
+  if (distance<6) 
+  {  
+    bot.sendMessage(m.chat_id, "Dustbin is full and it needs maintance"); 
+  }  
+ 
+}
